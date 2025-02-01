a759c64c17539b3549985402d3570da514d40a1c
Jollepoker <danielvedin@gmail.com>
2025-02-01 19:42:05 +0100

    Layout related fixes

--- assets/scripts/nepSweeper.js
+++ assets/scripts/nepSweeper.js
@@ -620,7 +620,7 @@ class NepSweeper {
             const nepSweeperStyle  = document.createElement('link');
             nepSweeperStyle.id   = 'nepSweeperStyle';
             nepSweeperStyle.rel  = 'stylesheet';
-            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.73';
+            nepSweeperStyle.href = './assets/styling/nepSweeperStyle.css?v=2.74';
             document.head.appendChild(nepSweeperStyle);
         }
 
