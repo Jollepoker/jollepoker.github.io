d592feffa082d71c2af51368312cdb1e89886805
Jollepoker <danielvedin@gmail.com>
2025-06-15 21:36:29 +0200

    Basic setup

---
+++ src/NepSchedule.vue
@@ -0,0 +1,16 @@
+<template>
+    <header>
+
+    </header>
+    <main>
+
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+    name: 'NepSchedule',
+});
+</script>
---
+++ src/main.ts
@@ -0,0 +1,6 @@
+import './assets/main.css'
+
+import { createApp } from 'vue'
+import NepSchedule from './NepSchedule.vue'
+
+createApp(NepSchedule).mount('#nepSchedule');
