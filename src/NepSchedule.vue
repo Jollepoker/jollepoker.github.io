b4052cb0325e50b4c0aefd74fc5b5bc465e996a6
Jollepoker <danielvedin@gmail.com>
2025-06-16 01:11:54 +0200

    Use dayjs for date handling

--- src/NepSchedule.vue
+++ src/NepSchedule.vue
@@ -1,6 +1,10 @@
 <template>
     <header>
-
+        <span class="nepSchedule-streamMonth">{{ targetDate.startOf('w').format('MMM').toLowerCase() }}</span>
+        <span class="nepSchedule-streamDay">{{ targetDate.startOf('w').date() }}</span>
+        <span class="nepSchedule-dateSeparator"> - </span>
+        <span class="nepSchedule-streamMonth">{{ targetDate.endOf('w').format('MMM').toLowerCase() }}</span>
+        <span class="nepSchedule-streamDay">{{ targetDate.endOf('w').date() }}</span>
     </header>
     <main>
 
@@ -9,8 +13,15 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import type { NepScheduleData } from './interfaces/data';
 
 export default defineComponent({
     name: 'NepSchedule',
+    data() {
+        return {
+            todaysDate: this.$dayjs(),
+            targetDate: this.$dayjs(),
+        } as NepScheduleData
+    },
 });
 </script>
--- src/main.ts
+++ src/main.ts
@@ -2,5 +2,22 @@ import './assets/main.css'
 
 import { createApp } from 'vue'
 import NepSchedule from './NepSchedule.vue'
+import dayjs from 'dayjs';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+import locale from 'dayjs/locale/en-gb';
 
-createApp(NepSchedule).mount('#nepSchedule');
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale(locale);
+
+const app = createApp(NepSchedule);
+app.config.globalProperties.$dayjs = dayjs;
+
+declare module 'vue' {
+    interface ComponentCustomProperties {
+        $dayjs: typeof dayjs
+    }
+}
+
+app.mount('#nepSchedule');
