f675cad847a56d4833c44b34120581f5a6a660e6
Jollepoker <danielvedin@gmail.com>
2025-06-21 17:26:42 +0200

    Disallow week switcher to go to future weeks

--- src/components/NepWeekSwitcher.vue
+++ src/components/NepWeekSwitcher.vue
@@ -2,12 +2,18 @@
     <div class="nepSchedule-WeekSwitcherWrapper">
         <span class="nepSchedule-weekSwitcherArrow" @click="minusOneWeek"><</span>
         <span class="nepSchedule-weekSwitcherWeek">Week: {{ targetDate.week() }}</span>
-        <span class="nepSchedule-weekSwitcherArrow" @click="plusOneWeek">></span>
+        <span
+            class="nepSchedule-weekSwitcherArrow"
+            :class="{ 'nepSchedule-weekSwitcherArrow-disabled': canPlusOneWeek }"
+            @click="plusOneWeek"
+        >
+            >
+        </span>
     </div>
 </template>
 
 <script lang="ts">
-import { Dayjs } from 'dayjs';
+import dayjs, { Dayjs } from 'dayjs';
 import { defineComponent } from 'vue';
 
 export default defineComponent({
@@ -19,12 +25,19 @@ export default defineComponent({
             required: true,
         }
     },
+    computed: {
+        canPlusOneWeek(): boolean {
+            return this.targetDate.add(1, 'week').isAfter(dayjs());
+        }
+    },
     methods: {
-        minusOneWeek() {
+        minusOneWeek(): void {
             this.$emit('changeTargetDate', this.targetDate.subtract(1, 'week'));
         },
-        plusOneWeek() {
-            this.$emit('changeTargetDate', this.targetDate.add(1, 'week'));
+        plusOneWeek(): void {
+            if (this.targetDate.add(1, 'week').isBefore(dayjs())) {
+                this.$emit('changeTargetDate', this.targetDate.add(1, 'week'));
+            }
         },
     }
 });
