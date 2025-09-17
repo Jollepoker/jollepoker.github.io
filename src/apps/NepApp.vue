9234464ac72cd0df4cd2f3d41df173492853b44a
Jollepoker <danielvedin@gmail.com>
2025-09-17 22:09:33 +0200

    Various fixes, replace background with webp

--- src/apps/NepApp.vue
+++ src/apps/NepApp.vue
@@ -1,7 +1,5 @@
 <template>
-    <video autoplay muted loop id="nep-background">
-        <source src="/assets/images/site/background.mp4" type="video/mp4" />
-    </video>
+    <img id="nep-background" src="/assets/images/site/background.webp" />
     <div id="nep-siteWrapper">
         <div id="nep-eyeWrapper">
             <img id="nep-eye" src="/assets/images/site/eye.webp" />
