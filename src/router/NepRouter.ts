8cf5fff58c7cf768fc0230cf5eceb4dd194b2943
Jollepoker <danielvedin@gmail.com>
2025-10-11 19:24:01 +0200

    Add page able to redirect to discord

--- src/router/NepRouter.ts
+++ src/router/NepRouter.ts
@@ -2,11 +2,13 @@ import { createWebHashHistory, createRouter } from 'vue-router';
 import NepSchedule from '@/apps/nepSchedule/NepSchedule.vue';
 import NepSweeper from '@/apps/nepSweeper/NepSweeper.vue';
 import NepTwitch from '@/apps/nepTwitch/NepTwitch.vue';
+import DiscordRedirect from '@/apps/discordRedirect/DiscordRedirect.vue';
 
 const routes = [
     { path: '/', name: 'NepSchedule', component: NepSchedule },
     { path: '/nepsweeper', name: 'NepSweeper', component: NepSweeper },
     { path: '/twitch/:streamer?', name: 'NepTwitch', props: true, component: NepTwitch },
+    { path: '/discord/:server?', name: 'DiscordRedirect', props: true, component: DiscordRedirect },
     { path: '/:pathMatch(.*)*', redirect: '/' },
 ];
 
