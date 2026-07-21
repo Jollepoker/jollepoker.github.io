6520489adc93e1ed2bac6f33c375ac4a99bac4af
Jollepoker <sadroulette@gmail.com>
2026-07-21 17:50:26 +0200

    Properly build each theme

---
+++ dist/assets/theme-main-CdjETh5d.js
@@ -0,0 +1,1 @@
+const s="/assets/main-BF3caCfn.css";export{s as _};
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,4 @@
 import { fileURLToPath, URL } from 'node:url';
-
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import vueDevTools from 'vite-plugin-vue-devtools';
@@ -16,4 +15,19 @@ export default defineConfig({
     optimizeDeps: {
         include: ['vue'],
     },
+    build: {
+        cssCodeSplit: true,
+        rollupOptions: {
+            output: {
+                manualChunks: (id) => {
+                    if (id.includes('/styling/')) {
+                        const match = id.match(/\/styling\/([^/]+)/);
+                        if (match) {
+                            return `theme-${match[1]}`;
+                        }
+                    }
+                }
+            }
+        }
+    }
 });
